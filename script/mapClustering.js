09eb2741fd47ad2b559339d8e168da88fac395ee
Arvind Sivakumar <96773379+Programmer-Arvind@users.noreply.github.com>
2024-03-23 23:50:26 +0530

    Just show relevant marker on search and hit enter to search

--- script/mapClustering.js
+++ script/mapClustering.js
@@ -2,8 +2,8 @@ import { hereApiKey } from "./secrets.js";
 import { touristPlaces } from "./touristPlaces.js";
 
 function startClustering(map, data) {
-    // First we need to create an array of DataPoint objects,
-    // for the ClusterProvider
+  // First we need to create an array of DataPoint objects,
+  // for the ClusterProvider
     var dataPoints = data.map(function (item) {
       return new H.clustering.DataPoint(item.latitude, item.longitude);
     });
@@ -39,17 +39,59 @@ var map = new H.Map(
     document.getElementById('mapContainer'),
     maptypes.vector.normal.map,
     {
-        zoom: 4.7,
-        center: { lng: 78.9629, lat: 22.6000  }, //20.5937° N, 78.9629° E
-        pixelRatio: window.devicePixelRatio || 1
+      zoom: 4.7,
+      center: { lng: 78.9629, lat: 22.6000  }, //20.5937° N, 78.9629° E
+      pixelRatio: window.devicePixelRatio || 1
     });
 
-var ui = H.ui.UI.createDefault(map, maptypes);
+var service = platform.getSearchService();
 
-var mapEvents = new H.mapevents.MapEvents(map);
-
-var behavior = new H.mapevents.Behavior(mapEvents);
-
-window.addEventListener('resize', () => map.getViewPort().resize());
-
-startClustering(map, touristPlaces);
+let markers = [];
+function search(){
+  console.log(markers);
+  if (markers.length !=0){
+    for( let i = markers.length -1; i >=0; i--){
+      map.removeObject(markers[i]);
+      markers.pop();
+    }
+  }
+  
+  service.geocode(
+    
+    {
+      q: document.getElementById("searchinput").value,
+    },
+    (result) => {
+      // Add a marker for each location found
+      
+      result.items.forEach((item) => {
+        let marker = new H.map.Marker(item.position);
+        markers.push(marker);
+        map.addObject(marker);
+        map.setCenter(item.position);
+        //console.log(item.position.lat, item.position.lng);
+      });
+    },
+    alert
+  );
+}
+searchbtn.addEventListener('click', e => {
+  search(); 
+});
+searchinput.addEventListener('keyup', function(event){
+  if (event.key == 'Enter'){
+    search();
+  }
+});
+    var ui = H.ui.UI.createDefault(map, maptypes);
+    
+    var mapEvents = new H.mapevents.MapEvents(map);
+    
+    var behavior = new H.mapevents.Behavior(mapEvents);
+    
+    window.addEventListener('resize', () => map.getViewPort().resize());
+    
+    startClustering(map, touristPlaces);
+    
+    
+    
